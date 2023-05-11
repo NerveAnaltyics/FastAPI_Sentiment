510aa3bc1fed2291479c108fc1a9227dac3c743a
NerveAnaltyics <125850329+NerveAnaltyics@users.noreply.github.com>
2023-05-11 16:02:06 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -33,4 +33,4 @@ async def getsentiment(text:str):
     
 
 if __name__ =="__main__":
-    uvicorn.run("main:app", host="0.0.0.0",port=8080,reload=True)
+    uvicorn.run("main:app", host="0.0.0.0",port=9000,reload=True)
